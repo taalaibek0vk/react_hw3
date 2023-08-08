0eab464b1f02eb8b9d7dc7f85b58a2f1a5ba1d6a
taalaibek0vk <taalaibekov1503@gmail.com>
2023-08-08 09:18:04 +0600

    Сделал дз7

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React, { useState } from 'react';
 
-function App() {
+export default function TodoList() {
   const [tasks, setTasks] = useState([]);
   const [inputValue, setInputValue] = useState('');
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const addTodDo = () => {
-    if (inputValue.trim() !== '') {
-      setTasks([...tasks, inputValue]);
+  const addTask = () => {
+    if (inputValue) {
+      setTasks([inputValue, ...tasks]);
       setInputValue('');
     }
   };
 
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const toggleTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = (
+      <span style={{ textDecoration: 'line-through' }}>{tasks[index]}</span>
+    );
+    setTasks(updatedTasks);
+  };
+
   return (
     <div>
-      <h1>Todo List</h1>
-      <input type="text" placeholder='Пиши задачу!' value={inputValue} onChange={handleInputChange} />
-      <button onClick={addTodDo}>Add</button>
-      <ol>
+      <h1>Список задач</h1>
+      <div className='container'>
+        <input
+        placeholder='Введи задачу!'
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addTask}>Добавить</button>
+      <ul>
         {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(index)}>Удалить</button>
+            <button onClick={() => toggleTask(index)}>Зачеркнуть</button>
+          </li>
         ))}
-      </ol>
+      </ul>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
